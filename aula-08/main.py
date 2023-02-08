f28382cb93509377d557a4fa51fa9c235cb9326f
Rafiere <rafapgbr@gmail.com>
2023-02-08 08:25:16 -0300

    feat: add lesson 08 code

---
+++ aula-08/main.py
@@ -0,0 +1,4 @@
+# O "functools" é uma biblioteca do Python que possui várias comparações implementadas, bastando
+# definirmos o "__eq__" e qualquer uma das outras dunder functions.
+
+# Basta utilizarmos a anotação "@total_ordering" sob a declaração da classe, e importar o "functools".
